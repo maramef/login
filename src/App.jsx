147c9821e1132b9d5b10dc4c9f728357d012a685
marame <fadelmarame082005@gmail.com>
2025-11-15 16:42:23 +0100

    fix: import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Login from './components/login'
+import Login from './components/Login'
 import Home from './components/Home'
 import SignIn from './components/SignIn'
 
